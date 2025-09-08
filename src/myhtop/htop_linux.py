465eab556ed37b40455f5320775d1ba169a5f3ef
Pablo Fraile Alonso <pablo@pablofraile.net>
2025-09-08 19:11:19 +0200

    build: ignore solution

--- src/myhtop/htop_linux.py
+++ src/myhtop/htop_linux.py
@@ -2,16 +2,19 @@ from typing import List
 
 from amsatop import Htop, Process
 
+from myhtop.procfs.proc_fs import ProcFS
+
 
 class HtopLinux(Htop):
     def __init__(self):
-        super(HtopLinux, self).__init__()
+        super().__init__()
+        self.procfs = ProcFS(proc_path=self.proc_folder)
 
     def get_processes(self) -> List[Process]:
-        return []
+        return self.procfs.processes
 
     def get_priorities(self) -> List[Process]:
-        return []
+        return self.procfs.priorities
 
     def get_hup(self) -> List[Process]:
-        return []
+        return self.procfs.nohup
